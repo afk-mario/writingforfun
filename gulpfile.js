ee6f872274d47be6689b81fd7b6474b7995f01f3
Arlefreak <hi@arlefreak.com>
2017-05-15 15:57:39 -0500

    Added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,164 +1,243 @@
 'use strict';
 
-const gulp        = require('gulp');
-const gutil       = require('gulp-util');
-const source      = require('vinyl-source-stream');
-const browserify  = require('browserify');
-const babelify    = require('babelify');
+const gulp = require('gulp');
+const fs = require('fs');
 const runSequence = require('run-sequence');
-const bower       = require('main-bower-files');
-const gulpif      = require('gulp-if');
-const lint        = require('gulp-eslint');
-const uglify      = require('gulp-uglify');
-const concat      = require('gulp-concat');
-const flatten     = require('gulp-flatten');
-const rename      = require('gulp-rename');
-const gulpFilter  = require('gulp-filter');
-const nib         = require('nib');
-const stylus      = require('gulp-stylus');
-const connect     = require('gulp-connect');
-const imagemin    = require('gulp-imagemin');
-const cleanCSS    = require('gulp-clean-css');
-const pngquant    = require('imagemin-pngquant');
-const gifsicle    = require('imagemin-gifsicle');
-const jpegtran    = require('imagemin-jpegtran');
-const svgo        = require('imagemin-svgo');
+const bower = require('main-bower-files');
+const gulpif = require('gulp-if');
+const lint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const flatten = require('gulp-flatten');
+const rename = require('gulp-rename');
+const gulpFilter = require('gulp-filter');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const gutil = require('gulp-util');
+const nib = require('nib');
+const stylus = require('gulp-stylus');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const gifsicle = require('imagemin-gifsicle');
+const jpegtran = require('imagemin-jpegtran');
+const realFavicon = require ('gulp-real-favicon');
+const FAVICON_DATA_FILE = 'faviconData.json';
 
-const DEBUG = process.env.NODE_ENV === 'production' ? false : true;
+// const svgo = require('imagemin-svgo');
+
+const DEBUG = process.env.NODE_ENV === 'production';
 const DEST_PATH = 'public/';
-const DEST_PATH_LIB = DEST_PATH + 'lib/';
 
 // grab libraries files from bower_components, minify and push in /public
-gulp.task('bower', ()=> {
-    var filterJs = gulpFilter('**/*.js', {
-        restore: true
-    });
-    var filterCss = gulpFilter('**/*.css', {
-        restore: true
-    });
-    var filterFont = gulpFilter(['**/*.eot', '**/*.woff', '**/*.ttf'],{
-        restore: true
-    });
-    var filterImg = gulpFilter(['**/*.jpg', '**/*.png', '**/*.svg', '**/*.jpeg', '**/*.gif'],{
-        restore: true
-    });
+gulp.task('bower', () => {
+  const jsFilter = gulpFilter('**/*.js', {
+    restore: true,
+  });
+  const cssFilter = gulpFilter('*.css', {
+    restore: true,
+  });
+  const fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
+  const destPath = `${DEST_PATH}lib`;
 
-    return gulp.src(bower({
-        debugging: true,
-        includeDev: true
-    }))
+  return gulp.src(bower({
+    debugging: true,
+    includeDev: true,
+  }))
 
-    // grab vendor js files from bower_components, minify and push in /public
-    .pipe(filterJs)
+  // grab vendor js files from bower_components, minify and push in /public
+    .pipe(jsFilter)
+    .pipe(gulp.dest(`${destPath}/js/`))
     .pipe(gulpif(!DEBUG, uglify()))
+    .pipe(concat('vendor.js'))
     .pipe(rename({
-        suffix: '.min'
+      suffix: '.min',
     }))
-    .pipe(gulp.dest(DEST_PATH_LIB + 'js/'))
-    .pipe(filterJs.restore)
+    .pipe(gulp.dest(destPath + '/js/'))
+    .pipe(jsFilter.restore)
 
-    // grab vendor css files from bower_components, minify and push in /public
-    .pipe(filterCss)
-    .pipe(gulp.dest(DEST_PATH_LIB + 'css/'))
-    .pipe(gulpif(!DEBUG, cleanCSS({compatibility: 'ie8'})))
+  // grab vendor css files from bower_components, minify and push in /public
+    .pipe(cssFilter)
+    .pipe(gulp.dest(destPath + '/css'))
+  // .pipe(minifycss())
     .pipe(rename({
-        suffix: '.min'
+      suffix: '.min',
     }))
-    .pipe(gulp.dest(DEST_PATH_LIB + 'css/'))
-    .pipe(filterCss.restore)
+    .pipe(gulp.dest(destPath + '/css'))
+    .pipe(cssFilter.restore)
 
-    // grab vendor font files from bower_components and push in /public
-    .pipe(filterFont)
+  // grab vendor font files from bower_components and push in /public
+    .pipe(fontFilter)
     .pipe(flatten())
-    .pipe(gulp.dest(DEST_PATH_LIB + 'fonts/'))
-
-    // grab vendor font files from bower_components and push in /public
-    .pipe(filterImg)
-    .pipe(flatten())
-    .pipe(gulp.dest(DEST_PATH_LIB + 'img/'));
+    .pipe(gulp.dest(destPath + '/fonts'));
 });
 
-gulp.task('browserify', ()=> {
-    return browserify()
+
+gulp.task('browserify', () => {
+  return browserify()
     .on('error', gutil.log)
     .require('./src/js/app.js', {
-        entry: true,
-        extensions: ['.js', 'jsx'],
-        debug: true
+      entry: true,
+      extensions: ['.js', 'jsx'],
+      debug: true,
     })
     .transform(babelify, {
-        presets: ['es2015', 'react', 'stage-2']
+      presets: ['es2015', 'react']
     })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest(DEST_PATH + 'js/'));
+    .pipe(gulp.dest(DEST_PATH + 'js'));
 });
 
-gulp.task('js', ()=> {
-    return gulp.src(['src/js/**/*.js', '!src/js/templates/**/*.js'])
-    .pipe(lint())
-    .pipe(lint.format())
-    .pipe(lint.failAfterError())
-    .pipe(gulpif(!DEBUG,uglify()))
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest(DEST_PATH + 'js/'))
-    .pipe(connect.reload());
+
+gulp.task('js', () => {
+  return gulp.src(['src/js/**/*.js', '!src/js/templates/**/*.js'])
+    .pipe(lint.format());
 });
 
-gulp.task('css', ()=> {
-    gulp.src('src/css/style.styl')
+gulp.task('css', () => {
+  gulp.src('src/css/style.styl')
     .pipe(stylus({
-        use:nib(),
-        compress: !DEBUG,
-        import:['nib']
+      use: nib(),
+      compress: !DEBUG,
+      import: ['nib'],
     }))
-    .pipe(gulp.dest(DEST_PATH + 'css/'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest(DEST_PATH + 'css/'));
 });
 
-gulp.task('img', ()=>{
-    gulp.src('src/img/**/*')
-    .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        use: [pngquant(), gifsicle(), jpegtran(), svgo()]
-    }))
-    .pipe(gulp.dest(DEST_PATH + 'img/'))
-    .pipe(connect.reload());
+gulp.task('img', () => {
+  gulp.src('src/img/**/*')
+    .pipe(gulp.dest(DEST_PATH + 'img'));
 });
 
-gulp.task('html', ()=> {
-    gulp.src('./src/*.html')
-    .pipe(gulp.dest(DEST_PATH))
-    .pipe(connect.reload());
-});
-
-gulp.task('files', ()=> {
-    gulp.src(['./src/**.*', '!./src/**.*.html', '!./src/**.*js', '!./src/img/'])
+gulp.task('html', () => {
+  gulp.src('./src/*.html')
     .pipe(gulp.dest(DEST_PATH));
 });
 
-gulp.task('react', ()=> {
-    runSequence('js', 'browserify');
+gulp.task('files', () => {
+  gulp.src(['./src/**.*', '!./src/**.*.html', '!./src/**.*js', '!./src/img/'])
+    .pipe(gulp.dest(DEST_PATH));
 });
 
-gulp.task('connect', ()=> {
-    connect.server({
-        root: DEST_PATH,
-        livereload: true,
-    });
+gulp.task('react', () => {
+  runSequence('js', 'browserify');
+});
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', (done) => {
+  realFavicon.generateFavicon({
+    masterPicture: 'src/img/favicon.png',
+    dest: DEST_PATH + 'img/favicons/',
+    iconsPath: '/img/favicons/',
+    design: {
+      ios: {
+        pictureAspect: 'backgroundAndMargin',
+        backgroundColor: '#ffffff',
+        margin: '14%',
+        assets: {
+          ios6AndPriorIcons: false,
+          ios7AndLaterIcons: false,
+          precomposedIcons: false,
+          declareOnlyDefaultIcon: true,
+        },
+        appName: 'ellugar.co',
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#000000',
+        onConflict: 'override',
+        assets: {
+          windows80Ie10Tile: true,
+          windows10Ie11EdgeTiles: {
+            small: true,
+            medium: true,
+            big: true,
+            rectangle: false,
+          },
+        },
+        appName: 'ellugar.co'
+      },
+      androidChrome: {
+        pictureAspect: 'noChange',
+        themeColor: '#000000',
+        manifest: {
+          name: 'ellugar.co',
+          display: 'standalone',
+          orientation: 'notSet',
+          onConflict: 'override',
+          declared: true
+        },
+        assets: {
+          legacyIcon: false,
+          lowResolutionIcons: false
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'blackAndWhite',
+        threshold: 10,
+        themeColor: '#000000'
+      }
+    },
+    settings: {
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false
+    },
+    versioning: {
+      paramName: 'v',
+      paramValue: 'rM3xw7xzqY'
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
+  });
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+  return gulp.src(['views/favicon.html'])
+    .pipe(
+      realFavicon.injectFaviconMarkups(
+        JSON.parse(
+          fs.readFileSync(FAVICON_DATA_FILE)
+        ).favicon.html_code
+      ))
+    .pipe(gulp.dest('views/'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', () => {
+  const currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+  realFavicon.checkForUpdates(currentVersion, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
+
+gulp.task('favicon', () => {
+  runSequence('generate-favicon', 'inject-favicon-markups');
 });
 
 gulp.task('init', ['css', 'bower', 'react', 'img', 'html', 'files']);
 
-gulp.task('watch', ['css', 'react', 'img', 'html', 'connect'], ()=> {
-    gulp.watch('src/css/**/*.styl', ['css']);
-    gulp.watch('src/js/**/*.js', ['react']);
-    gulp.watch('src/js/**/*.jsx', ['react']);
-    gulp.watch('src/img/**/*', ['img']);
-    gulp.watch('src/*.html', ['html']);
+gulp.task('watch', () => {
+  gulp.watch('src/css/**/*.styl', ['css']);
+  gulp.watch('src/js/**/*.js', ['react']);
+  gulp.watch('src/js/**/*.jsx', ['react']);
+  gulp.watch(['src/img/**/*', '!src/img/favicon.png'], ['img']);
+  gulp.watch('src/*.html', ['html']);
 });
 
-gulp.task('default', ()=> {
-    runSequence('init', 'watch');
+gulp.task('default', () => {
+  runSequence('init', 'watch');
 });
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-'use strict';
+/* eslint no-console: ["error", { allow: ["warn", "error", "log" ] }] */
 
+const express = require('express');
 const path = require('path');
-require('dotenv').config({path: path.resolve(__dirname, '.env')});
-const express = require("express");
-const app = express();
+require('dotenv').config({ path: path.resolve(__dirname, '.env'), silent: true });
 const bodyParser = require('body-parser');
 const errorHandler = require('errorhandler');
 const methodOverride = require('method-override');
+const ejs = require('ejs');
+
 const port = parseInt(process.env.PORT, 10) || 8000;
 const DEV = process.env.NODE_ENV !== 'production';
 
-app.get("/", (req, res)=> {
-    res.redirect("index.html");
-});
+const app = express();
 
-app.use(methodOverride());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.engine('.html', ejs.__express);
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(errorHandler({
-    dumpExceptions: true,
-    showStack: true
+app.get('/*', require('./routes').index);
+
+app.use(methodOverride());
+
+app.use(bodyParser.urlencoded({
+  extended: true,
 }));
 
-app.listen(port, '0.0.0.0', function onStart(err) {
-    var info = `==> Listening on port ${port}. Open up http://0.0.0.0:${port}/ DEV: ${DEV}`
-    if (err) {
-        console.log(err);
-    }
-    console.info(info);
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true,
+}));
+
+app.listen(port, '0.0.0.0', (err) => {
+  const info = `==> Listening on port ${port}. Open up http://0.0.0.0:${port}/ DEV: ${DEV}`;
+  if (err) {
+    console.error(err);
+  }
+  console.log(info);
 });
